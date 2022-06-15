4cfc16e6815cf39c4e718437f4e58a081ada539b
Matheus Antonio <matheus.antonio208@hotmail.com>
2022-06-14 21:56:01 -0300

    utilizando sass

--- webpack.config.js
+++ webpack.config.js
@@ -35,8 +35,8 @@ module.exports = {
         },
       },
       {
-        test: /\.css$/,
-        use: ['style-loader', 'css-loader'],
+        test: /\.scss$/,
+        use: ['style-loader', 'css-loader', 'sass-loader'],
       },
     ],
   },
