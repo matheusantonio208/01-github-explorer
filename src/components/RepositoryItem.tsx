a6140c8acad3c6b98d8a6fdda937b3910c81a94a
Matheus Antonio <matheus.antonio208@hotmail.com>
2022-06-18 02:19:47 -0300

    feat: finalizando aplicação

--- src/components/RepositoryItem.tsx
+++ src/components/RepositoryItem.tsx
@@ -1,5 +1,14 @@
 import React from 'react';
-export function RepositoryItem(props) {
+
+interface RepositoryItemProps {
+  repository: {
+    name: string;
+    description: string;
+    html_url: string;
+  };
+}
+
+export function RepositoryItem(props: RepositoryItemProps) {
   return (
     <li>
       <strong>{props.repository.name ?? 'Default'}</strong>
--- src/components/RepositoryList.tsx
+++ src/components/RepositoryList.tsx
@@ -3,10 +3,14 @@ import { RepositoryItem } from './RepositoryItem';
 
 import '../styles/repositories.scss';
 
-// https://api.github.com/orgs/rocketseat/repos
+interface Repository {
+  name: string;
+  description: string;
+  html_url: string;
+}
 
 export function RepositoryList() {
-  const [repositories, setRepositories] = useState([]);
+  const [repositories, setRepositories] = useState<Repository[]>([]);
 
   useEffect(() => {
     fetch('https://api.github.com/orgs/rocketseat/repos')
@@ -19,7 +23,7 @@ export function RepositoryList() {
       <h1>Lista de Repositórios</h1>
       <ul>
         {repositories.map((repository) => (
-          <RepositoryItem key={repository.id} repository={repository} />
+          <RepositoryItem key={repository.name} repository={repository} />
         ))}
       </ul>
     </section>
